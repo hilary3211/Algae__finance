3b1ba0a8d5c3c88292f79e15958e90014fb68bab
hilary3211 <80103905+hilary3211@users.noreply.github.com>
2024-05-01 18:50:50 +0100

    Update index.js

--- index.js
+++ index.js
@@ -31,5 +31,7 @@ app.get('/refer/:RefAdd', async function(req, res) {
     res.render('index.ejs', {CTAdress : contractAddress, refid : req.params.RefAdd, rate : rate, owner : Owner_Address});
 });
 
-
-app.listen("3000");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
